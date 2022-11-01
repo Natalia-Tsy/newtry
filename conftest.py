371dd915c354bc8242a7b7e856f28fc937747afa
”Natalia <n.i.tsyrelchuk@gmail.com>
2022-11-01 18:31:43 +0500

    update conftest

--- conftest.py
+++ conftest.py
@@ -2,10 +2,13 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as ec
 
 @pytest.fixture(scope='function')
 def browser():
     print('\nstart browser...')
+    options = webdriver.ChromeOptions()
+    options.headless = True
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     yield browser
     print('\nquit browser...')
