a4e5e20110094668105d599cf1ed54dcabc93688
”Natalia <n.i.tsyrelchuk@gmail.com>
2022-11-04 14:10:15 +0500

    conf

--- conftest.py
+++ conftest.py
@@ -4,14 +4,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def browser():
-    print('\nstart browser...')
+    print("\nstart browser...")
     options = webdriver.ChromeOptions()
     options.add_argument("--window-size=1600,1080")
     options.headless = True
-    browser = webdriver.Chrome(service=Service(
-        ChromeDriverManager().install()), options=options)
+    browser = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=options
+    )
     yield browser
-    print('\nquit browser...')
+    print("\nquit browser...")
     browser.quit()
