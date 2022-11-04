e2e1c1a3cd3680f9d25d454b57dcd0d1adbe9dde
”Natalia <n.i.tsyrelchuk@gmail.com>
2022-11-04 09:26:03 +0500

    add black

--- conftest.py
+++ conftest.py
@@ -7,10 +7,10 @@ from selenium.webdriver.support import expected_conditions as ec
 @pytest.fixture(scope='function')
 def browser():
     print('\nstart browser...')
-    options = webdriver.ChromeOptions()
-    options.add_argument("--window-size=1600,1080")
-    options.headless = True
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("--window-size=1600,1080")
+    # options.headless = True
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install())) #options=options)
     yield browser
     print('\nquit browser...')
     browser.quit()
